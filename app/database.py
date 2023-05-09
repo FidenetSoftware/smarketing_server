d0a9768c2dade2422b7a4e4ee658dcad9636ea8e
Nerea Sarasola <nereafidenet@gmail.com>
2023-05-09 10:04:45 +0200

    [UPDATE] Nueva estructura

---
+++ app/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+DATABASE_URI : str = os.getenv('DATABASE_URI')
+
+engine = create_engine(DATABASE_URI, connect_args = {"check_same_thread": False})
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
+
+
+Base = declarative_base()
+
