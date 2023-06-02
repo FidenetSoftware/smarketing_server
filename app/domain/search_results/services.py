31d35d87ef5fe0c50472cc8eaae292fe312a3a0b
Nerea Sarasola <nereafidenet@gmail.com>
2023-06-02 12:25:46 +0200

    [UPDATE] Limpieza de código

--- app/domain/search_results/services.py
+++ app/domain/search_results/services.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session, joinedload
 from . import models
 from ..text import model as text_model
 
-#Get the results by id
+#Get the text table's data by search_id
 async def get_results_by_searchId(db: Session, id: int):
  
     query = db.query(models.Search_Results).options(joinedload(models.Search_Results.text))
