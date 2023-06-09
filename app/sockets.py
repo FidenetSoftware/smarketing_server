5a28139da103793b0c2952bf48fea6faecd8dfa9
Nerea Sarasola <nereafidenet@gmail.com>
2023-06-09 15:51:34 +0200

    [UPDATE] He conseguido hacer que se escuchara a un evento

--- app/sockets.py
+++ app/sockets.py
@@ -8,9 +8,19 @@ sio_app = socketio.ASGIApp(
     sio_server
 )
 
+
+
 @sio_server.event
 async def connect(sid, environ, auth):
     print(sid, 'connected')
     await sio_server.emit('login', {'join': sid})
 
+@sio_server.on('message')
+async def recive_message(sid, data):
+    print(data)
 
+
+
+@sio_server.event
+async def disconnect(sid):
+    print('disconnect ', sid)
