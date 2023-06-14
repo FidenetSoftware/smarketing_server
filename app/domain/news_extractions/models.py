2d61c656397a70bbef95a57b1b36bb90a8b27ca5
Nerea Sarasola <nereafidenet@gmail.com>
2023-06-14 16:15:33 +0200

    [UPDATE] Modelo de la tabla news_extractions creada

---
+++ app/domain/news_extractions/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Text, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+
+from ...database import Base
+
+class News_Extractions(Base):
+    __tablename__ = "news_extractions"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    news_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    news_date = Column(DateTime(timezone=True), unique=True, index=True)
+    title = Column(String, unique=True, index=True)
+    excerpt = Column(Text, unique=True, index=True)
+    summary = Column(Text, unique=True, index=True)
+    author = Column(String, unique=True, index=True)
+    link = Column(String, unique=True, index=True)
+    clean_url = Column(String, unique=True, index=True)
+    media = Column(Text, unique=True, index=True)
+    raw_data = Column(Text, unique=True, index=True)
+    search_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+
+    youtube = relationship("Search", back_populates="youtube_searches")
